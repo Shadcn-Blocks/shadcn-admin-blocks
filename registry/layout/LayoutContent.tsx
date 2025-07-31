697452ea807a07922001c91e794c101b15e72438
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2025-07-31 13:23:56 +0200

    feat(layout): add no padding route option

--- registry/layout/LayoutContent.tsx
+++ registry/layout/LayoutContent.tsx
@@ -1,5 +1,20 @@
+import { cn } from '@/lib/utils'
+import { useMatches } from '@tanstack/react-router'
 import { PropsWithChildren } from 'react'
 
+declare module '@tanstack/react-router' {
+  interface StaticDataRouteOption {
+    noLayoutContentPadding?: boolean
+  }
+}
+
 export const LayoutContent = ({ children }: PropsWithChildren) => {
-  return <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
+  const matches = useMatches()
+  const currentMatch = matches[matches.length - 1]
+  const staticData = currentMatch?.staticData
+  return (
+    <div className={cn('flex flex-1 flex-col gap-4', !staticData?.noLayoutContentPadding && 'p-4')}>
+      {children}
+    </div>
+  )
 }
